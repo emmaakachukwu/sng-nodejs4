e8e041c6ce0d70a6f53ddbd430e1c713dbe00782
= <=>
2020-05-10 12:45:06 +0100

    removed env config

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express()
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const favicon = require('express-favicon');
-require('dotenv').config({path: "./.env"})
+// require('dotenv').config({path: "./.env"})
 
-app.use(favicon(__dirname + '/public/favicon.png'));
+// app.use(favicon(__dirname + '/public/favicon.png'));
+
+app.set( 'port', ( process.env.PORT || 5000 ));
 // ROUTES
 const studentRoutes = require("./routes/students");
 const tutorRoutes = require("./routes/tutors");
@@ -14,7 +16,7 @@ const adminRoutes = require("./routes/admin");
 mongoose.connect("mongodb+srv://emmaakachukwu:782009ace@cluster0-v3dg0.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then(
     result => {
         console.log("Database connected");
-        app.listen(process.env.PORT || 3000);
+        app.get('port')
     }
 ).catch(
     err => console.log(err)
