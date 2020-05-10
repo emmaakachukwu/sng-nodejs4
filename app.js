dc653ef3b1cc20b0795c178fc71d2c8b47fa9343
emmanuel akachukwu <emmanuelakachukwu1@gmail.com>
2020-05-10 12:29:51 +0100

    debugging

--- app.js
+++ app.js
@@ -14,18 +14,18 @@ const adminRoutes = require("./routes/admin");
 mongoose.connect("mongodb+srv://emmaakachukwu:782009ace@cluster0-v3dg0.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then(
     result => {
         console.log("Database connected");
-        app.listen(process.env.PORT || 4200);
+        app.listen(process.env.PORT || 3000);
     }
 ).catch(
     err => console.log(err)
 );
+
+app.get('/', (req, res) => {
+    res.send('This is the home page')
+})
 
 app.use(bodyParser.json())
 
 app.use('/api/v1/students', studentRoutes)
 app.use('/api/v1/tutors', tutorRoutes)
 app.use('/api/v1/admin', adminRoutes)
-
-app.get('/', (req, res) => {
-    res.send('This is the home page')
-})
